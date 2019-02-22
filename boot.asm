dc96b3cc174d1c2c2ccc17b7a7cfb00226281afc
defau1t <robin5693@gmail.com>
2019-02-22 07:15:16 -0600

    Prints Hello World to console

--- boot.asm
+++ boot.asm
@@ -1,5 +1,22 @@
+[ORG 0x7c00]
+
+  xor ax, ax
+  mov ds, ax
+
+  mov si, msg
+  cld
+ch_loop:lodsb
+  or al, al
+  jz hang
+  mov ah, 0x0E
+  mov bh, 0
+  int 0x10
+  jmp ch_loop
+
 hang:
   jmp hang
+
+msg   db 'Hello World', 13, 10, 0
 
   times 510-($-$$) db 0
   db 0x55
