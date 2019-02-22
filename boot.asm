34ca246039b094d99c9853581c0fb56b0f991de9
defau1t <robin5693@gmail.com>
2019-02-22 07:17:37 -0600

    Made bios_print a 'proper' function

--- boot.asm
+++ boot.asm
@@ -2,21 +2,27 @@
 
   xor ax, ax
   mov ds, ax
+  cld
 
   mov si, msg
-  cld
-ch_loop:lodsb
-  or al, al
-  jz hang
-  mov ah, 0x0E
-  mov bh, 0
-  int 0x10
-  jmp ch_loop
+  call bios_print
 
 hang:
   jmp hang
 
 msg   db 'Hello World', 13, 10, 0
+
+bios_print:
+  lodsb
+  or al, al
+  jz done
+  mov ah, 0x0E
+  mov bh, 0
+  int 0x10
+  jmp bios_print
+done:
+  ret
+
 
   times 510-($-$$) db 0
   db 0x55
