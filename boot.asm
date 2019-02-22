a33f71dcdb84f154869b74ec5795da317e574e4d
defau1t <robin5693@gmail.com>
2019-02-22 07:37:55 -0600

    Modularized code

--- boot.asm
+++ boot.asm
@@ -1,30 +1,20 @@
 ; vim: ft=nasm
+
+%include "biosprint.inc"
+
 [ORG 0x7c00]
 
-  xor ax, ax
-  mov ds, ax
-  cld
+	xor ax, ax
+	mov ds, ax
+	cld
 
-  mov si, msg
-  call bios_print
+	BiosPrint msg
 
 hang:
-  jmp hang
+	jmp hang
 
 msg   db 'Hello World', 13, 10, 0
 
-bios_print:
-  lodsb
-  or al, al
-  jz done
-  mov ah, 0x0E
-  mov bh, 0
-  int 0x10
-  jmp bios_print
-done:
-  ret
-
-
-  times 510-($-$$) db 0
-  db 0x55
-  db 0xAA
+	times 510-($-$$) db 0
+	db 0x55
+	db 0xAA
