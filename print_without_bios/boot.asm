8f48a28c634c23e4bcc6095a2d077bcadd9a5327
defau1t <robin5693@gmail.com>
2019-02-22 08:43:56 -0600

    Text prints without bios support

--- print_without_bios/boot.asm
+++ print_without_bios/boot.asm
@@ -9,13 +9,30 @@ jmp main
 main:
 	xor ax, ax
 	mov ds, ax
+	mov ss, ax
 	cld
 
+	mov ax, 0xb800
+	mov es, ax
+
 	NoBiosPrint msg
+
+	mov ax, 0xb800
+	mov gs, ax
+	mov bx, 0x0000
+	mov ax, [gs:bx]
+
+	mov word [reg16], ax
+	call printreg16
 
 hang:
 	jmp hang
 
+xpos db 0
+ypos db 0
+hexstr db '0123456789ABCDEF'
+outstr16 db '0000', 0
+reg16 dw 0
 msg   db 'Hello World', 13, 10, 0
 
 	times 510-($-$$) db 0
